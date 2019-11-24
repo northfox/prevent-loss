a27b9390037b1a2b60c99f07119776c84a04a9b5
northfox <vulpes.engineer@gmail.com>
2019-11-25 08:45:18 +0900

    Add dir for users,bags

---
+++ pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="users">userId: {{ id }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
