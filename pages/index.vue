76ea2d6274753d22c89cf9d1fe49f633421e4091
northfox <vulpes.engineer@gmail.com>
2019-11-24 21:28:47 +0900

    Update project settings at default files

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        prevent-loss
-      </h1>
-      <h2 class="subtitle">
-        My laudable Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <logo />
+    <h1 class="title">
+      prevent-loss
+    </h1>
   </div>
 </template>
 
