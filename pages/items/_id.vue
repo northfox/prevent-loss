34132ea66565ad7582a4a8825ad75a24ac2a0887
northfox <vulpes.engineer@gmail.com>
2019-11-29 08:23:28 +0900

    Add item edit

--- pages/items/_id.vue
+++ pages/items/_id.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="item">
     <h2>Item detail</h2>
-    <ul>
+    <button @click="isEditable = false" v-if="isEditable" type="button">save</button>
+    <button @click="isEditable = true" v-else type="button">edit</button>
+    <ul v-if="isEditable">
+      <li>itemId: {{ item.id }}</li>
+      <li>
+        name:
+        <input v-model="item.name" type="text" />
+      </li>
+      <li>
+        desc:
+        <textarea v-model="item.desc"></textarea>
+      </li>
+      <li>
+        amount:
+        <input v-model="item.amount" type="number" />
+      </li>
+      <li>
+        min:
+        <input v-model="item.min" type="number" />
+      </li>
+      <li>
+        max:
+        <input v-model="item.max" type="number" />
+      </li>
+    </ul>
+    <ul v-else>
       <li>itemId: {{ item.id }}</li>
       <li>name: {{ item.name }}</li>
       <li>desc: {{ item.desc }}</li>
@@ -16,6 +41,7 @@
 export default {
   data() {
     return {
+      isEditable: false,
       item: {
         id: this.$route.params.id,
         name: 'hoge',
